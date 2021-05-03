d8bf38aa0e4852e1a4d0231cebddd0220a9d48b9
Paul Matthews <>
2021-05-03 17:49:55 +0000

    failed attempt

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
   // test
-	return "Hello world!";
+	return "Helld!";
 
 }
  
